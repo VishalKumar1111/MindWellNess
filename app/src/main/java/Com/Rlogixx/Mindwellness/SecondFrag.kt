21bf7f6091f8ec5a846aac5fc52b601015c3f377
VISHAL <singh01vishal01@gmail.com>
2022-05-02 16:32:23 +0530

    new

--- app/src/main/java/Com/Rlogixx/Mindwellness/SecondFrag.kt
+++ app/src/main/java/Com/Rlogixx/Mindwellness/SecondFrag.kt
@@ -1,11 +1,23 @@
 package Com.Rlogixx.Mindwellness
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.AttributeSet
 import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class SecondFrag:Fragment(R.layout.second_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val medic=view.findViewById<ImageView>(R.id.img_medic)
+
+        medic.setOnClickListener {
+            it.findNavController().navigate(R.id.action_secondfrag_to_thirdFrag)
+        }
     }
 }
