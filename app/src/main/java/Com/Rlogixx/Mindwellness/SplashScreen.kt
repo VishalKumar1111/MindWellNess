1cb2335c892caee6f6f0f333333cbf79f8b5fa28
VISHAL <singh01vishal01@gmail.com>
2022-05-02 17:09:35 +0530

    new

---
+++ app/src/main/java/Com/Rlogixx/Mindwellness/SplashScreen.kt
@@ -0,0 +1,33 @@
+package Com.Rlogixx.Mindwellness
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val textView4=findViewById<TextView>(R.id.textView4)
+        val imageView3=findViewById<ImageView>(R.id.imageView3)
+        imageView3.alpha= 0f
+        imageView3.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i= Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+        textView4.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i= Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+}
---
+++ app/src/main/java/Com/Rlogixx/Mindwellness/ThirdFrag.kt
@@ -0,0 +1,13 @@
+package Com.Rlogixx.Mindwellness
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class ThirdFrag:Fragment(R.layout.thirdfrag) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
