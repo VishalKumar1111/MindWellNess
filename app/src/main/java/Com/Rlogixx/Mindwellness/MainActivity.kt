3eead1b0d1f84816da89a25658b59ac6614aa1a3
VISHAL <singh01vishal01@gmail.com>
2022-05-02 18:03:28 +0530

    new

--- app/src/main/java/Com/Rlogixx/Mindwellness/MainActivity.kt
+++ app/src/main/java/Com/Rlogixx/Mindwellness/MainActivity.kt
@@ -1,5 +1,6 @@
 package Com.Rlogixx.Mindwellness
 
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,7 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(android.R.color.holo_green_dark)))
+
     }
+
+
 
 
 }
