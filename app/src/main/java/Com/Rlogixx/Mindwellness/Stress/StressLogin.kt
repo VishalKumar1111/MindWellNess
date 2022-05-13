c960e48662d1d785102b21c45a3a64360adabe4d
VISHAL <singh01vishal01@gmail.com>
2022-05-13 17:47:43 +0530

    UPdated

---
+++ app/src/main/java/Com/Rlogixx/Mindwellness/Stress/StressLogin.kt
@@ -0,0 +1,15 @@
+package Com.Rlogixx.Mindwellness.Stress
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import Com.Rlogixx.Mindwellness.R
+
+
+class StressLogin : Fragment(R.layout.fragment_stress_login) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
