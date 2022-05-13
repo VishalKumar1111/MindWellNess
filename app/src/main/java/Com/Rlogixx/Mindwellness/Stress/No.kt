c40268c45bd64920204e6ae92dd17a95ca96264f
Vishal <singh01vishal01@gmail.com>
2022-05-13 15:00:36 +0530

    dd

---
+++ app/src/main/java/Com/Rlogixx/Mindwellness/Stress/No.kt
@@ -0,0 +1,14 @@
+package Com.Rlogixx.Mindwellness.Stress
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import Com.Rlogixx.Mindwellness.R
+
+class No : Fragment(R.layout.fragment_no) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+ }
