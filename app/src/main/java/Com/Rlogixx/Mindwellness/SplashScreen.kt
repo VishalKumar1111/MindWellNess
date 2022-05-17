a74dd762d71d581d183ae85fe983672529741ceb
VISHAL <singh01vishal01@gmail.com>
2022-05-17 17:46:19 +0530

    commit

--- app/src/main/java/Com/Rlogixx/Mindwellness/SplashScreen.kt
+++ app/src/main/java/Com/Rlogixx/Mindwellness/SplashScreen.kt
@@ -12,7 +12,6 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        val textView4=findViewById<TextView>(R.id.textView4)
         val imageView3=findViewById<ImageView>(R.id.imageView3)
         imageView3.alpha= 0f
         imageView3.animate().setDuration(1500).alpha(1f).withEndAction{
